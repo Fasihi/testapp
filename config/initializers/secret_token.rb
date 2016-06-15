df9f349b13a0de9f14db61260ee84e0e66360ce8
Minaam Fasihi <minaam.fasihi@square63.com>
2016-06-15 12:02:25 +0500

    Implemented Polymorphic associations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polymorphic::Application.config.secret_token = 'f17af3ad78913eeb42c7aa220b170f1656ee8a9e808cb01f0c6a23edc9cbbb29290b9f2c6391fe467d27cf856ecc4c829cd8d5ba840d896bedfaeefcd3d82975'
