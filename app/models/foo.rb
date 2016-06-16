86dbda63edd1e51ba90f2669c99116910c0df6c7
Fasihi <minaam.fasihi@square63.com>
2016-06-16 14:43:58 +0500

    New text file

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -2,4 +2,4 @@ class Foo < ActiveRecord::Base
 	Somelksjdlfjsldjkfklj changes here and there and lets live in peace
 	sdfjslakdjfklsjlkfjlsjjj
 	there is so much painn out there
-end
+endsaklsjdklfjklsskld
